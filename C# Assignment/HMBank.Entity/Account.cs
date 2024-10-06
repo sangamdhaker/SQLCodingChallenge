4c3f65aaf92ddf5fd5f3f6fb1253cf7693d969c7
sangamdhaker <144347284+sangamdhaker@users.noreply.github.com>
2024-10-07 00:27:25 +0530

    C# Assignment
    C# Assignment of HMBank

---
+++ C# Assignment/HMBank.Entity/Account.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMBank.Entity
+{
+    public abstract class Account
+    {
+        public int AccountNumber { get; set; }
+        public string AccountType { get; set; }
+        public double Balance { get; set; }
+
+        public abstract void Deposit(double amount);
+        public abstract void Withdraw(double amount);
+    }
+
+    public class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public override void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public override void Withdraw(double amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds.");
+            }
+        }
+
+        public void CalculateInterest()
+        {
+            Balance += Balance * (InterestRate / 100);
+        }
+    }
+
+    public class CurrentAccount : Account
+    {
+        public double OverdraftLimit { get; set; }
+
+        public override void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public override void Withdraw(double amount)
+        {
+            if (Balance + OverdraftLimit >= amount)
+            {
+                Balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Withdrawal exceeds overdraft limit.");
+            }
+        }
+    }
+
+}
---
+++ C# Assignment/HMBank.Entity/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMBank.Entity
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+        public int AccountId { get; set; }
+        public string TransactionType { get; set; }
+        public double Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+    }
+}
