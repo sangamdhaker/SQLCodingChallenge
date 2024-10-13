934841735c2180936052f7d33f1089319e05b76b
sangamdhaker <144347284+sangamdhaker@users.noreply.github.com>
2024-10-13 17:53:09 +0530

    Add files via upload

---
+++ C# Assignment/Task 1 to 6/Task 1/ConditionalStatements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input data
+            Console.Write("Enter your credit score: ");
+            int creditScore = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your annual income: $");
+            double annualIncome = double.Parse(Console.ReadLine());
+
+            // Check loan eligibility
+            if (creditScore > 700 && annualIncome >= 50000)
+            {
+                Console.WriteLine("You are eligible for a loan.");
+            }
+            else
+            {
+                Console.WriteLine("You are not eligible for a loan.");
+            }
+        }
+    }
+}
---
+++ C# Assignment/Task 1 to 6/Task 3/LoopStructures/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopStructures
+{
+    internal class Program
+    {
+        static double CalculateFutureBalance(double initialBalance, double annualInterestRate, int years)
+        {
+            return initialBalance * Math.Pow(1 + (annualInterestRate / 100), years);
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter number of customers: ");
+            int numCustomers = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numCustomers; i++)
+            {
+                Console.Write("\nEnter initial balance: $");
+                double initialBalance = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter annual interest rate (%): ");
+                double annualInterestRate = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter number of years: ");
+                int years = int.Parse(Console.ReadLine());
+
+                double futureBalance = CalculateFutureBalance(initialBalance, annualInterestRate, years);
+                Console.WriteLine($"Future balance after {years} years: ${futureBalance:F2}");
+            }
+        }
+    }
+}
