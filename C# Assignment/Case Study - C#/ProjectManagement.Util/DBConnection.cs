10d284bf35bdc91a8eae9687de9a11e51c320320
sangamdhaker <144347284+sangamdhaker@users.noreply.github.com>
2024-10-14 14:16:52 +0530

    Case Study

---
+++ C# Assignment/Case Study - C#/ProjectManagement.Util/DBConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace ProjectManagementSystem.Util
+{
+
+    public static class DBConnection
+    {
+        private static SqlConnection _connection;
+
+        public static SqlConnection GetConnection(string connectionString)
+        {
+            if (_connection == null)
+            {
+                _connection = new SqlConnection(connectionString);
+            }
+            return _connection;
+        }
+    }
+}
